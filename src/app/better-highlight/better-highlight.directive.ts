f96a07fdaa8a0e8331b7761bfbff1fb3564d77f4
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-26 22:07:15 -0400

    95.using the Renderer to build a better attribute directive

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  // accessing element : private elRef: ElementRef
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // setStyle method set style of some elements
+    // elRef(which element want to style, "which style", "value assign for this property")
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+  }
+
+}
+// after saving this add to app component
